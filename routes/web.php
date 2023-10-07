8ced3b4d5d9e2950c1c7dde22b722e8fd7755151
Ivan19Aldana <ivanuchi189@gmail.com>
2023-10-07 09:18:37 -0600

    Correción de errores

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route:: view('/','auth.login');
-route::get("/ver",[\App\Http\Controllers\empleadoscontroller::class,'listado']);
+route::get("/ver",[\App\Http\Controllers\empleadoscontroller::class,'listado'])->name("Listar");
 route::get("/mas",[\App\Http\Controllers\empleadoscontroller::class,'crear']);
 route::get("/GUARDAR",[\App\Http\Controllers\empleadoscontroller::class,'save'])->name("save");
 route::get("/EDITAR/{id}",[\App\Http\Controllers\empleadoscontroller::class,'modificar'])->name('modificar');
